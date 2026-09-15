e752ee6af8a82939ae7278293b461da735a29816
Karthik M <karthik915062@gmail.com>
2026-09-15 12:47:33 +0530

    Add files via upload

---
+++ carry_lookahead_adder.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.09.2026 11:21:50
+// Design Name: 
+// Module Name: carry_lookahead_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module carry_lookahead_adder(
+    input [3:0] A,
+    input [3:0]B,
+    input Cin,
+    output [3:0]sum,
+    output Cout
+    );
+    
+ wire [3:0] P,G;
+ wire  C1,C2,C3;
+    
+assign P = A^B;
+assign G = A&B;
+
+assign C1= G[0] | (P[0] & Cin);
+assign C2= G[1] | (P[1] & G[0]) |(P[1] & P[0] & Cin);
+assign C3= G[2] | (P[2] & G[1]) | (P[2] & P[1]&G[0]) | (P[2] & P[1] & P[0] & Cin);
+assign Cout = G[3] | (P[3] & C3);
+
+assign sum[0] = P[0] ^ Cin;
+assign sum[1] = P[1] ^ C1;
+assign sum[2] = P[2] ^ C2;
+assign sum[3] = P[3] ^ C3;
+
+endmodule
